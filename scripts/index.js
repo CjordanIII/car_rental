4a10040e6c552804e75290429b95242407eb8e81
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-03 13:12:55 -0500

    updated

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+    - Basic Car Rental is $29.99 per day
+    - There is a 30% surcharge if you are under 25
+    - Option costs
+        - Electric Toll Tag $3.95
+        - GPS $2.95
+        - Roadside Assistance $2.95
+    - Data to display
+        - Car rental cost
+        - Options cost
+        - under 25 surcharge cost
+        - Total due
+*/
+
+window.onload = function (event) {
+  let rentalForm = document.querySelector("#rentalForm");
+  event.preventDefault();
+  alert("hello from car rental fees");
+};
